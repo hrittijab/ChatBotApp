bc2e8cc9969f2eca4dc585bd09ecf7424eb1b844
hrittijab <hrittija2001@gmail.com>
2025-01-11 18:51:51 -0700

    Basic hi-hello chat updated

---
+++ app/src/main/java/com/example/chatbotapp/Message.java
@@ -0,0 +1,20 @@
+package com.example.chatbotapp;
+
+public class Message {
+    private String text;
+    private boolean isUserMessage;
+
+    public Message(String text, boolean isUserMessage) {
+        this.text = text;
+        this.isUserMessage = isUserMessage;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public boolean isUserMessage() {
+        return isUserMessage;
+    }
+}
+
